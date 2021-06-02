935f5541cc2f6ed3226a0ae553b37095482433a3
sameer-j <samir4nov@gmail.com>
2021-06-02 21:19:38 +0530

    learn repo: starting with basic counter

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Learn from './modules/learn';
+
+const App = () => {
+  return <Learn />;
+};
+
+export default App;
---
+++ src/modules/learn/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Learn = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <View>
+      <Text>Test</Text>
+      <Button
+        title={'Counter:' + counter}
+        onPress={() => setCounter(counter + 1)}
+      />
+      <Button title="Reset Counter" onPress={() => setCounter(0)} />
+    </View>
+  );
+};
+
+export default Learn;
