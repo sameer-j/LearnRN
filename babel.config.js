6cc0232f1a23cde6be91ed8925e246b9178cc126
Sameer Jain <samir4nov@gmail.com>
2021-10-01 13:46:15 +0530

    web working without material navigator
    Directly switch to LearnRoutes and it works. But navigation back button is not visible, but clickable.
    probably native things arent working.

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,9 @@ module.exports = {
         runtime: 'automatic',
       },
     ],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
   env: {
     production: {
